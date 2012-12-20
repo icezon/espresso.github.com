2c77d8db24aa8f9ca5eda59c75f97c7a1d27125b
slivu <slivuz@gmail.com>
2012-12-20 14:47:51 +0200

    0

---
+++ builder.rb
@@ -0,0 +1,34 @@
+require 'cgi'
+require "rdiscount"
+require "tilt"
+
+layout = 'view/layout.erb'
+
+File.open 'index.html', 'w' do |fh|
+  fh << Tilt::ERBTemplate.new(layout).render do
+    Tilt::ERBTemplate.new('view/index.erb').render
+  end
+end
+
+Dir['../espresso/**/*.md'].each do |file|
+
+  md = File.read(file).
+    gsub(/```(\w+)/, "<pre>").
+    gsub(/```/, "</pre>").
+    gsub(/https\:\/\/github\.com\/slivu\/espresso#tutorial/, '#')
+
+  engine = Tilt::RDiscountTemplate.new { md }
+
+  html = Tilt::ERBTemplate.new(layout).render do
+    engine.render.
+      gsub(/\<h(\d)\>([^<]*)\<\/h\d\>/i) do
+        d, w = $1, $2
+        id = w.downcase.gsub(/\W/, '-')
+        "<h#{d} id='#{id}'><a href='##{id}'>#{w}</a></h#{d}>"
+      end
+  end
+
+  File.open(File.basename(file, File.extname(file)) << '.html', 'w') do |fh|
+    fh << html
+  end
+end
